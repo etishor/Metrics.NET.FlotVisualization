ce87208fc4d14dc0fa6705d7f269f54bcf128468
Iulian Margarintescu <etishor@gmail.com>
2014-04-18 22:24:46 +0300

    fix watch performance issue on windows

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,7 @@ module.exports = function (grunt) {
 				files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
 				tasks: ['newer:jshint:all'],
 				options: {
+					spawn: false,
 					livereload: true
 				}
 			},
